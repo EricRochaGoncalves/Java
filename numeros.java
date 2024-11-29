60c0a32218e477797426fd90796cc0352a00a1f6
Eric Gonçalves <166281170+EricRochaGoncalves@users.noreply.github.com>
2024-11-29 18:31:58 -0300

    Atualizar o numeros.java

--- numeros.java
+++ numeros.java
@@ -7,15 +7,33 @@ public class numeros {
 
         Scanner teclado = new Scanner(System.in);
         do {
-            System.out.print(" Leão da receita federal: Digite seus gastos: ");
+            System.out.print("Leão da receita federal: Digite seus gastos: ");
             n = teclado.nextInt();
             s += n;
+
             System.out.print("Quer ir em frente? hahahaah [S/N] ");
             resp = teclado.next();
 
+            if (resp.equalsIgnoreCase("S")) {
+                System.out.println("Você disse sim! Bora continuar roubando seu dinheiro hahaha!");
+            } else {
+                if (resp.equalsIgnoreCase("N")) {
+                    System.out.println("Você disse não! Tá querendo fugir, né? Vai dar ruim!");
+                } else {
+                    System.out.println("Resposta inválida, digite apenas [S/N].");
+                }
+            }
+
+            // Pausa para que a mensagem seja lida antes de continuar
+            try {
+                Thread.sleep(2000); // Pausa por 2 segundos
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
         } while (resp.equalsIgnoreCase("S")); 
-        
-        System.out.println("A soma de todos os impostps é " + s + " se ferrou hahaha compre Bitcoin");
+
+        System.out.println("A soma de todos os impostos é " + s + ". Se ferrou hahaha compre Bitcoin!");
         teclado.close(); 
     }
 }
