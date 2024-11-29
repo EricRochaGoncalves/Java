eab15190f1bffd3a641f10f935803d38149a8bbd
Eric Gonçalves <166281170+EricRochaGoncalves@users.noreply.github.com>
2024-11-29 12:30:15 -0300

    Add files via upload

---
+++ numeros.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class numeros {
+    public static void main(String[] args) {
+        int n, s = 0;
+        String resp;
+
+        Scanner teclado = new Scanner(System.in);
+        do {
+            System.out.print(" Leão da receita federal: Digite seus gastos: ");
+            n = teclado.nextInt();
+            s += n;
+            System.out.print("Quer ir em frente? hahahaah [S/N] ");
+            resp = teclado.next();
+
+        } while (resp.equalsIgnoreCase("S")); 
+        
+        System.out.println("A soma de todos os impostps é " + s + " se ferrou hahaha compre Bitcoin");
+        teclado.close(); 
+    }
+}
+
---
+++ repeat_01.java
@@ -0,0 +1,11 @@
+public class repeat_01 {
+    public static void main(String[] args) {
+        int cc = 0;
+        do {
+            System.out.println(cc);
+            cc++;
+
+        } while (cc<1000);
+    }
+    
+}
