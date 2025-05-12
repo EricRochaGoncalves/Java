2a822e661c51b78457c1f7d7c27a6bcd281f9fdb
Eric Gonçalves <166281170+EricRochaGoncalves@users.noreply.github.com>
2025-05-12 19:27:54 -0300

    Create Arrays03.java

---
+++ DevDojo/Arrays03.java
@@ -0,0 +1,18 @@
+package DevDojo;
+
+public class Arrays03 {
+    public static void main(String[] args) {
+
+        int [] numeros2 = {1,2,3,4,5};
+
+        System.out.println("calma ae tô testando: ");
+
+
+        for (int num : numeros2) {
+            System.out.println(num);
+            
+        }
+
+
+    }        
+}
