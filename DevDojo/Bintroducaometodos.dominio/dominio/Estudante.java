f832150f4ff25652b5b8507f4eb44a6499973311
Eric Gonçalves <166281170+EricRochaGoncalves@users.noreply.github.com>
2025-05-15 17:26:30 -0300

    Update Estudante.java

--- DevDojo/Bintroducaometodos.dominio/dominio/Estudante.java
+++ DevDojo/Bintroducaometodos.dominio/dominio/Estudante.java
@@ -5,5 +5,10 @@ public class Estudante {
     public int idade;
     public char sexo;
     
-    
+    public void imprime(){
+        System.out.println("Imprimindo os dados: ");
+        System.out.println(this.nome);
+        System.out.println(this.idade);
+        System.out.println(this.sexo);
+    }
 }
