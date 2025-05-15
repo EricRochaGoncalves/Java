7a1fb229a9f4c3620317ff449f3a2a18a280a65e
Eric Gonçalves <166281170+EricRochaGoncalves@users.noreply.github.com>
2025-05-15 17:58:48 -0300

    Update Calculadora.java

--- DevDojo/Bintroducaometodos.dominio/dominio/Calculadora.java
+++ DevDojo/Bintroducaometodos.dominio/dominio/Calculadora.java
@@ -37,13 +37,32 @@ public class Calculadora {
 
     }
 
-    public void alteraDoisNumeros (int num1, int num2){
+    public void alteraDoisNumeros(int num1, int num2) {
         num1 = 99;
         num2 = 32;
-        System.out.println("Dentro do 'alteraDoisNumeros' " );
-        System.out.println("num1: " + num1 );
+        System.out.println("Dentro do 'alteraDoisNumeros' ");
+        System.out.println("num1: " + num1);
         System.out.println("num2:" + num2);
 
-        
+    }
+
+    public void somaArray(int[] numeros) {
+        int soma = 0;
+        for (int num : numeros) {
+            soma += num;
+
+        }
+        System.out.println(soma);
+    }
+
+    public void SomaVarArgs(int... numeros) {
+        int soma = 0;
+        for (int num : numeros) {
+            soma += num;
+
+        }
+        System.out.println(soma);
+
     }
 }
+
