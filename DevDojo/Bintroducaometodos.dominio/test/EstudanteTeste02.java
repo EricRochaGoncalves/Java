ba7dcaf6744a989d6ab9e0c7723d11855e02763e
Eric Gonçalves <166281170+EricRochaGoncalves@users.noreply.github.com>
2025-05-15 17:27:29 -0300

    Add files via upload

---
+++ DevDojo/Bintroducaometodos.dominio/test/EstudanteTeste02.java
@@ -0,0 +1,22 @@
+package test;
+
+import dominio.Estudante;
+
+public class EstudanteTeste02 {
+    public static void main(String[] args) {
+
+        Estudante estudante01 = new Estudante();
+        Estudante estudante02 = new Estudante();
+
+        estudante01.nome = "Leonardo";
+        estudante01.idade = 13;
+        estudante01.sexo = 'M';
+
+        estudante02.nome = "Eduarda";
+        estudante02.idade = 10;
+        estudante02.sexo = 'F';
+
+        estudante01.imprime();
+        estudante02.imprime();
+    }
+}
