64df3222d573426c841189ea30ab24a3b8cea6d5
Eric Gonçalves <166281170+EricRochaGoncalves@users.noreply.github.com>
2025-05-16 15:21:46 -0300

    Update and rename CalculadoraTest02.java to CalculadoraTeste02.java

--- DevDojo/Bintroducaometodos.dominio/test/CalculadoraTeste02.java
+++ DevDojo/Bintroducaometodos.dominio/test/CalculadoraTeste02.java
@@ -2,7 +2,7 @@ package test;
 
 import dominio.Calculadora02;
 
-public class CalculadoraTest02 {
+public class CalculadoraTeste02 {
     public static void main(String[] args) {
         Calculadora02 calc02 = new Calculadora02();
         calc02.miniCalculadora();
