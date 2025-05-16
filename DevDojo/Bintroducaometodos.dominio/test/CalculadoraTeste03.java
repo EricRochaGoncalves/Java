0507d5496a4bc8478c3141c0e262d27f3198ab74
Eric Gonçalves <166281170+EricRochaGoncalves@users.noreply.github.com>
2025-05-16 15:22:23 -0300

    Update and rename CalculadoraTest03.java to CalculadoraTeste03.java

--- DevDojo/Bintroducaometodos.dominio/test/CalculadoraTeste03.java
+++ DevDojo/Bintroducaometodos.dominio/test/CalculadoraTeste03.java
@@ -2,7 +2,7 @@ package test;
 
 import dominio.Calculadora03;
 
-public class CalculadoraTest03 {
+public class CalculadoraTeste03 {
     public static void main(String[] args) {
         Calculadora03 calcX2 = new Calculadora03();
         calcX2.MultiplicarDoisNumeros(10, 15);
