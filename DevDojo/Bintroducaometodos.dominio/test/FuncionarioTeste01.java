89243316206fad6ff48c73fc1368991d3e55bf8f
Eric Gonçalves <166281170+EricRochaGoncalves@users.noreply.github.com>
2025-05-19 02:39:27 -0300

    Update FuncionarioTeste01.java

--- DevDojo/Bintroducaometodos.dominio/test/FuncionarioTeste01.java
+++ DevDojo/Bintroducaometodos.dominio/test/FuncionarioTeste01.java
@@ -5,9 +5,9 @@ import dominio.Funcionario;
 public class FuncionarioTeste01 {
     public static void main(String[] args) {
         Funcionario funcionario = new Funcionario();
-        funcionario.nome = "Josemar";
-        funcionario.idade = 49;
-        funcionario.salarios = new double[]{1200, 987.32, 2000};
+        funcionario.setNome("Josemar"); 
+        funcionario.setIdade(70);
+        funcionario.setSalarios(new double[]{1200, 987.32, 2000}); 
 
         funcionario.imprime();
         
