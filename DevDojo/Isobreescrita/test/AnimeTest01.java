98047d17e6dae8408bd1e701a7ae5b0cafd66861
Eric Gonçalves <166281170+EricRochaGoncalves@users.noreply.github.com>
2025-05-24 01:13:53 -0300

    Add files via upload

---
+++ DevDojo/Isobreescrita/test/AnimeTest01.java
@@ -0,0 +1,10 @@
+package Isobreescrita.test;
+
+import Isobreescrita.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Naruto");
+        System.out.println(anime);
+    }
+}
